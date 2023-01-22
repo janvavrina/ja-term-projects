80657534f30ea691cd41e6483a88a09d05c43e1d
Jan Vavřina <JAN.VAVRINA@seznam.cz>
2023-01-22 19:00:43 +0100

    Many to many Team on Tournament

--- projekt3/src/main/java/cz/mendelu/ja/xvavrina/projekt3/Team/Team.java
+++ projekt3/src/main/java/cz/mendelu/ja/xvavrina/projekt3/Team/Team.java
@@ -1,5 +1,6 @@
 package cz.mendelu.ja.xvavrina.projekt3.Team;
 
+import cz.mendelu.ja.xvavrina.projekt3.Tournament.Tournament;
 import cz.mendelu.ja.xvavrina.projekt3.User.User;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -13,6 +14,8 @@ public class Team {
     @Id
     @Column(name = "name")
     private String name;
+    @ManyToMany(mappedBy = "teamsOnTournament")
+    private List<Tournament> participatedTournaments;
 
     @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
     private List<User> players;
--- projekt3/src/main/java/cz/mendelu/ja/xvavrina/projekt3/Tournament/Tournament.java
+++ projekt3/src/main/java/cz/mendelu/ja/xvavrina/projekt3/Tournament/Tournament.java
@@ -2,11 +2,13 @@ package cz.mendelu.ja.xvavrina.projekt3.Tournament;
 
 import com.electronwill.nightconfig.core.conversion.PreserveNotNull;
 import cz.mendelu.ja.xvavrina.projekt3.Location;
+import cz.mendelu.ja.xvavrina.projekt3.Team.Team;
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "tournaments")
@@ -15,6 +17,13 @@ public class Tournament {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @ManyToMany
+    @JoinTable(
+            name = "TeamOnTournament",
+            joinColumns = @JoinColumn(name = "id", insertable = false,updatable = false),
+            inverseJoinColumns = @JoinColumn(name = "name", insertable = false,updatable = false))
+    private List<Team> teamsOnTournament;
 
     @Column(name = "date")
     @Nonnull
