51c1867a92a3b542ffe6f4c3812acec10a14441f
Jan Vavřina <JAN.VAVRINA@seznam.cz>
2023-01-22 16:07:04 +0100

    Migration - insert users

---
+++ projekt3/src/main/resources/db/migration/V2__insert-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id,first_name, last_name, username, email)
+VALUES ('7f905a0b-bb6e-11e3-9e8f-000000000000','John', 'Doe', 'johndoe123', 'johndoe123@gmail.com'),
+       ('7f905a0b-bb6e-11e3-9e8f-000000000001','Jane', 'Smith', 'janesmith456', 'janesmith456@gmail.com'),
+       ('7f905a0b-bb6e-11e3-9e8f-000000000002','Bob', 'Johnson', 'bobjohnson789', 'bobjohnson789@gmail.com');
