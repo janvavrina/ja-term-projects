3d2f0643ccf77273c4def0779e2bcdf5c7a784d9
Jan Vavřina <111298849+janvavrina@users.noreply.github.com>
2023-02-24 10:43:56 +0100

    Delete projekt1/cv11_letecka-posta directory

--- projekt1/cv11_letecka-posta/src/main/java/cz/mendelu/ja/leteckaposta/country/CityService.java
+++
@@ -1,7 +0,0 @@
-package cz.mendelu.ja.leteckaposta.country;
-
-import org.springframework.stereotype.Service;
-
-@Service
-class CityService {
-}
--- projekt1/cv11_letecka-posta/src/main/resources/db/migration/V0.0.1.1__Create_table_borders.sql
+++
@@ -1,5 +0,0 @@
-create table boarders (
-    country_cca3 char(3),
-    neighbour_cca3 char(3),
-    primary key (country_cca3, neighbour_cca3)
-)
